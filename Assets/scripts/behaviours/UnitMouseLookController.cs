c3965e1a5fe7e4d776ccef0e926f44548cf205c9
Pavel <nnnemesis.gm@gmail.com>
2016-03-05 12:31:38 +0300

    direction control splited for player and npcs

---
+++ Assets/scripts/behaviours/UnitMouseLookController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UnitMouseLookController : MonoBehaviour, ITireEventListener {
+
+    private BaseMoveState BaseMoveState;
+    private IEventTire EventTire;
+
+    void Start()
+    {
+        EventTire = GetComponent<IEventTire>();
+        BaseMoveState = GetComponent<BaseMoveState>();
+        EventTire.AddEventListener(TireEventType.ChangedDirectionEvent, this);
+    }
+
+    void Update()
+    {
+        var camera = Camera.main;
+        var screenMousePosition = Input.mousePosition;
+        var worldMousePosition = camera.ScreenToWorldPoint(screenMousePosition);
+        worldMousePosition.z = 0;
+        if(worldMousePosition.x - transform.position.x >= 0)
+        {
+            BaseMoveState.Direction = 1;
+        }
+        else
+        {
+            BaseMoveState.Direction = -1;
+        }
+    }
+
+    public void OnTireEvent(TireEvent ev)
+    {
+        if(ev.Type == TireEventType.ChangedDirectionEvent)
+        {
+            OnChangedDirectionEvent((ChangedDirectionEvent)ev);
+        }
+    }
+
+    void OnChangedDirectionEvent(ChangedDirectionEvent e)
+    {
+        var rotation = transform.rotation;
+        if (e.NewDirection > 0)
+        {
+            rotation.SetLookRotation(Vector3.forward);
+        }
+        else
+        {
+            rotation.SetLookRotation(Vector3.back);
+        }
+        transform.rotation = rotation;
+    }
+
+    
+    
+}
---
+++ Assets/scripts/behaviours/UnitWalkLookController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UnitWalkLookController : MonoBehaviour, ITireEventListener {
+
+    private BaseMoveState BaseMoveState;
+    private IEventTire EventTire;
+
+    void Start()
+    {
+        EventTire = GetComponent<IEventTire>();
+        BaseMoveState = GetComponent<BaseMoveState>();
+        EventTire.AddEventListener(TireEventType.ChangedMoveStateEvent, this);
+        EventTire.AddEventListener(TireEventType.ChangedDirectionEvent, this);
+    }
+
+    public void OnTireEvent(TireEvent ev)
+    {
+        if(ev.Type == TireEventType.ChangedMoveStateEvent)
+        {
+            OnChangedMoveStateEvent((ChangedMoveStateEvent)ev);
+        }
+        else if (ev.Type == TireEventType.ChangedDirectionEvent)
+        {
+            OnChangedDirectionEvent((ChangedDirectionEvent)ev);
+        }
+    }
+
+    void OnChangedMoveStateEvent(ChangedMoveStateEvent e)
+    {
+        var moveState = BaseMoveState.MoveState;
+        if (moveState == MoveState.Right)
+        {
+            BaseMoveState.Direction = 1;
+        }
+        else if(moveState == MoveState.Left)
+        {
+            BaseMoveState.Direction = -1;
+        }
+    }
+
+    void OnChangedDirectionEvent(ChangedDirectionEvent e)
+    {
+        var rotation = transform.rotation;
+        if (e.NewDirection > 0)
+        {
+            rotation.SetLookRotation(Vector3.forward);
+        }
+        else
+        {
+            rotation.SetLookRotation(Vector3.back);
+        }
+        transform.rotation = rotation;
+    }
+
+    
+    
+}
